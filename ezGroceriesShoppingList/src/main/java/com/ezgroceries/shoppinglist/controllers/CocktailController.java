2a9b9d5a84952c9c0780ccfed4961e505477a27b
BobieDobie <bo.dobbelaere@hotmail.com>
2023-03-07 15:50:11 +0100

    lab-03-external-api

--- ezGroceriesShoppingList/src/main/java/com/ezgroceries/shoppinglist/controllers/CocktailController.java
+++ ezGroceriesShoppingList/src/main/java/com/ezgroceries/shoppinglist/controllers/CocktailController.java
@@ -1,45 +1,56 @@
 package com.ezgroceries.shoppinglist.controllers;
 
+import com.ezgroceries.shoppinglist.feingclients.CocktailDBClient;
+import com.ezgroceries.shoppinglist.models.Cocktail;
+import com.ezgroceries.shoppinglist.models.CocktailDBResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class CocktailController {
 
+    private final CocktailDBClient cocktailClient;
     private static final Logger log = LoggerFactory.getLogger(CocktailController.class);
 
+    public CocktailController(CocktailDBClient cocktailClient) {
+        this.cocktailClient = cocktailClient;
+    }
+
     @GetMapping("/cocktails")
-    public String listCocktails(@RequestParam("search") String search) {
-        return "[\n" +
-                "    {\n" +
-                "        \"cocktailId\": \"23b3d85a-3928-41c0-a533-6538a71e17c4\",\n" +
-                "        \"name\": \"Margerita\",\n" +
-                "        \"glass\": \"Cocktail glass\",\n" +
-                "        \"instructions\": \"Rub the rim of the glass with the lime slice to make the salt stick to it. Take care to moisten..\",\n" +
-                "        \"image\": \"https://www.thecocktaildb.com/images/media/drink/wpxpvu1439905379.jpg\",\n" +
-                "        \"ingredients\": [\n" +
-                "            \"Tequila\",\n" +
-                "            \"Triple sec\",\n" +
-                "            \"Lime juice\",\n" +
-                "            \"Salt\"\n" +
-                "        ]\n" +
-                "    },\n" +
-                "    {\n" +
-                "        \"cocktailId\": \"d615ec78-fe93-467b-8d26-5d26d8eab073\",\n" +
-                "        \"name\": \"Blue Margerita\",\n" +
-                "        \"glass\": \"Cocktail glass\",\n" +
-                "        \"instructions\": \"Rub rim of cocktail glass with lime juice. Dip rim in coarse salt..\",\n" +
-                "        \"image\": \"https://www.thecocktaildb.com/images/media/drink/qtvvyq1439905913.jpg\",\n" +
-                "        \"ingredients\": [\n" +
-                "            \"Tequila\",\n" +
-                "            \"Blue Curacao\",\n" +
-                "            \"Lime juice\",\n" +
-                "            \"Salt\"\n" +
-                "        ]\n" +
-                "    }\n" +
-                "]";
+    public List<Cocktail> listCocktails(@RequestParam("search") String search) {
+        // call Feign Client
+        CocktailDBResponse dbResponse = cocktailClient.searchCocktails(search);
+        // isolate drinks as feign responses
+        List<CocktailDBResponse.DrinkResource> drinks = dbResponse.getDrinks();
+        // create list of cocktails in our data model
+        List<Cocktail> cocktailList = new ArrayList<>();
+        for (CocktailDBResponse.DrinkResource drink : drinks) {
+            // make cocktail
+            Cocktail cocktail = new Cocktail();
+            cocktail.setCocktailId(drink.getIdDrink());
+            cocktail.setName(drink.getStrDrink());
+            cocktail.setGlass(drink.getStrGlass());
+            cocktail.setInstructions(drink.getStrInstructions());
+            cocktail.setImage(drink.getStrDrinkThumb());
+            // prepare ingredients
+            List<String> ingredients = new ArrayList<>(); // for cocktail
+            String[] strIngredients = new String[] {drink.getStrIngredient1(), drink.getStrIngredient2(), drink.getStrIngredient3(), // from drink
+                                                    drink.getStrIngredient4(), drink.getStrIngredient5(), drink.getStrIngredient6(),
+                                                    drink.getStrIngredient7(), drink.getStrIngredient8(), drink.getStrIngredient9(),
+                                                    drink.getStrIngredient10(), drink.getStrIngredient11(), drink.getStrIngredient12(),
+                                                    drink.getStrIngredient13(), drink.getStrIngredient14(), drink.getStrIngredient15()};
+            for (String strIngredient : strIngredients) {
+                if (strIngredient != null) { ingredients.add(strIngredient); }
+            }
+            cocktail.setIngredients(ingredients);
+            cocktailList.add(cocktail);
+        }
+        return cocktailList;
     }
 }
--- ezGroceriesShoppingList/src/test/java/com/ezgroceries/shoppinglist/ControllerTests.java
+++ ezGroceriesShoppingList/src/test/java/com/ezgroceries/shoppinglist/ControllerTests.java
@@ -1,13 +1,22 @@
 package com.ezgroceries.shoppinglist;
 
+import com.ezgroceries.shoppinglist.feingclients.CocktailDBClient;
+import com.ezgroceries.shoppinglist.models.CocktailDBResponse;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 //We could refactor these tests into a test class for each controller bv: @WebMvcTest(controllers=CocktailController.class)
@@ -17,6 +26,9 @@ public class ControllerTests {
 
     @Autowired
     MockMvc mockMvc;
+
+    @MockBean
+    CocktailDBClient cocktailDBClient;
 
     @Test
     public void testGetList() throws Exception {
@@ -106,6 +118,8 @@ public class ControllerTests {
 
     @Test
     public void testGetAllCocktails() throws Exception {
+
+        // expected return; could've made variables for the drink parameters to avoid copy pasting them twice
         String cocktails = "[\n" +
                 "    {\n" +
                 "        \"cocktailId\": \"23b3d85a-3928-41c0-a533-6538a71e17c4\",\n" +
@@ -135,10 +149,44 @@ public class ControllerTests {
                 "    }\n" +
                 "]";
 
+        // configure mocks: drinks, List<drinks>, cocktailDBresponse     builders might've been nice to use
+        CocktailDBResponse.DrinkResource drink1 = new CocktailDBResponse.DrinkResource();
+        drink1.setIdDrink("23b3d85a-3928-41c0-a533-6538a71e17c4");
+        drink1.setStrDrink("Margerita");
+        drink1.setStrGlass("Cocktail glass");
+        drink1.setStrInstructions("Rub the rim of the glass with the lime slice to make the salt stick to it. Take care to moisten..");
+        drink1.setStrDrinkThumb("https://www.thecocktaildb.com/images/media/drink/wpxpvu1439905379.jpg");
+        drink1.setStrIngredient1("Tequila");
+        drink1.setStrIngredient2("Triple sec");
+        drink1.setStrIngredient3("Lime juice");
+        drink1.setStrIngredient4("Salt");
+        CocktailDBResponse.DrinkResource drink2 = new CocktailDBResponse.DrinkResource();
+        drink2.setIdDrink("d615ec78-fe93-467b-8d26-5d26d8eab073");
+        drink2.setStrDrink("Blue Margerita");
+        drink2.setStrGlass("Cocktail glass");
+        drink2.setStrInstructions("Rub rim of cocktail glass with lime juice. Dip rim in coarse salt..");
+        drink2.setStrDrinkThumb("https://www.thecocktaildb.com/images/media/drink/qtvvyq1439905913.jpg");
+        drink2.setStrIngredient1("Tequila");
+        drink2.setStrIngredient2("Blue Curacao");
+        drink2.setStrIngredient3("Lime juice");
+        drink2.setStrIngredient4("Salt");
+
+        List<CocktailDBResponse.DrinkResource> drinks = new ArrayList<>();
+        drinks.add(drink1);
+        drinks.add(drink2);
+
+        CocktailDBResponse cocktailDBResponse = new CocktailDBResponse();
+        cocktailDBResponse.setDrinks(drinks);
+
+        // mock the feign client
+        when(cocktailDBClient.searchCocktails("Russian")).thenReturn(cocktailDBResponse);
+
+        // perform test
         mockMvc.perform(get("/cocktails").param("search", "Russian").accept("application/json"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType("application/json"))
                 .andExpect(content().json(cocktails));
+
     }
 
 }
